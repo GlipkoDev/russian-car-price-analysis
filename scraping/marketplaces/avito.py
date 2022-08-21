0c4f06bc131c44b0c05ecde55dbce76377428dc6
Gleb Kazakov <gleb.kazakov5832@gmail.com>
2022-08-21 13:48:09 +0700

    Made a marketplace template and created Avito parser inherited from it
    - Created base parser class named Marketplace in marketplace.py which is a template for future parsers
    - Wrapped up work in Avito playground in a new Avito marketplace class inherited from base Marketplace class
    - Replaced data classes with dictionaries
    - Made folder that will contain final script for server, moved there subfolder with playground notebooks, added package "marketplaces" with marketplace parsers.
    - Ignored data.csv classes that are created as a result of parser's work
    - Ignored all .pyc files that come from marketplaces package

---
+++ scraping/marketplaces/avito.py
@@ -0,0 +1,63 @@
+from .marketplace import Marketplace
+
+class Avito(Marketplace):
+    
+    def __init__(self, marketplace_name, url_to_parse):
+        super().__init__(marketplace_name, url_to_parse)
+        
+    def get_offers_on_page(self, page):
+        return page.select('div[itemtype="http://schema.org/Product"]')
+    
+    def get_price(self, offer):
+        return float(offer.select_one('div[class*=price]').get_text().replace('\xa0', '')[:-1])
+    
+    # Title has car's name and year, so I save it inside object to use in future (get_name called before get_year)
+    def get_name(self, offer):
+        self.title = offer.select_one('div[class*=title]').get_text()
+        return self.title.split(', ')[0]
+    
+    def get_year(self, offer):
+        return int(self.title.split(', ')[1])
+    
+    # Same situation as with name nad year in a title, but now 5 params are in the same block (get_mileage called first)
+    def get_mileage(self, offer):
+        car_params = offer.select_one('div[class*=autoParams]')
+        
+        # If offer is vip, then additional info is on the upper line and separated from main params with <br>,
+        # because vip offers stand together in a row of three
+        # Otherwise, if offer is not vip, it have its own line, 
+        # and extra info is in the same line with main params divided by ', '
+        for line_break in offer.findAll('br'):     
+            line_break.replaceWith(', ')
+            
+        car_params = car_params.get_text().replace('\xa0', '').split(', ')
+            
+        self.car_params = car_params[-5:] # Sometimes there is more car params like condition 'Битый' and they go before 5 main params
+        return float(self.car_params[0][:-2])
+    
+    # Same situation: modification_info will be used in get_horsepower (get_engine_capacity called before get_horsepower)
+    def get_engine_capacity(self, offer):         
+        modification = self.car_params[1]
+        self.modification_info = modification.split(' ')
+        return float(self.modification_info[0])
+    
+    def get_horsepower(self, offer):
+        return float(self.modification_info[2][1:-5]) # remove brackets and postfix 'л.с.'
+
+    def get_body_type(self, offer):
+        return self.car_params[2]
+    
+    def get_drive_type(self, offer):
+        return self.car_params[3]
+    
+    def get_engine_type(self, offer):
+        return self.car_params[4]
+    
+    def get_offer_location(self, offer):
+        return offer.select_one('div[class*=geo]').get_text()
+    
+    def is_offer_vip(self, offer):
+        return 'items-vip' in offer.parent['class'][0]
+    
+    def get_offer_url(self, offer):
+        return 'https://avito.ru/' + offer.select_one('a[data-marker=item-title]')['href']
---
+++ scraping/marketplaces/marketplace.py
@@ -0,0 +1,130 @@
+# Scraping
+import requests
+from bs4 import BeautifulSoup as bs
+
+# Moving to newer TSL protocol
+import ssl
+from requests.adapters import HTTPAdapter
+from requests.packages.urllib3.poolmanager import PoolManager
+from requests.packages.urllib3.util import ssl_
+
+# Scraping timestamp
+from datetime import date
+
+# Saving scraped info
+import csv
+import os.path
+
+class TlsAdapter(HTTPAdapter):
+
+    CIPHERS = """ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-SHA384:ECDHE-ECDSA-AES256-SHA384:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-SHA256:AES256-SHA"""
+
+    def __init__(self, ssl_options=0, **kwargs):
+        self.ssl_options = ssl_options
+        super(TlsAdapter, self).__init__(**kwargs)
+
+    def init_poolmanager(self, *pool_args, **pool_kwargs):
+        ctx = ssl_.create_urllib3_context(ciphers=self.CIPHERS, cert_reqs=ssl.CERT_REQUIRED, options=self.ssl_options)
+        self.poolmanager = PoolManager(*pool_args, ssl_context=ctx, **pool_kwargs)
+
+class Marketplace():
+    
+    headers = ['price', 'name', 'year', 'mileage', 
+               'engine_capacity', 'horsepower', 'body_type', 'drive_type', 'engine_type', 
+               'offer_location', 'is_offer_vip', 'offer_url', 'marketplace', 'scraping_time']
+    data_file = 'data.csv'
+    
+    def __init__(self, marketplace_name, url_to_parse):
+
+        self.marketplace_name = marketplace_name
+        self.url_to_parse = url_to_parse
+
+        self.session = requests.session()
+        adapter = TlsAdapter(ssl.OP_NO_TLSv1 | ssl.OP_NO_TLSv1_1)
+        self.session.mount("https://", adapter)
+    
+    def parse_all_pages(self):
+        raise Exception('Not Implemented')
+    
+    def parse_page(self, page):
+        response = self.session.get(self.url_to_parse.format(page))
+
+        if response.status_code == 200:
+            page = bs(response.content, 'html.parser')
+            offers = self.get_offers_on_page(page)
+            cars_info = []
+            for offer in offers:
+                car_info = self.parse_offer(offer)
+                cars_info.append(car_info)
+            self.save_cars_info(cars_info)
+        else:
+            raise Exception('Response code differs from 200')
+
+    def get_offers_on_page(self, page):
+        raise Exception('Not Implemented')
+    
+    def parse_offer(self, offer):
+        return {
+            'price' : self.get_price(offer),
+            'name' : self.get_name(offer),
+            'year' : self.get_year(offer),
+            'mileage' : self.get_mileage(offer),
+            'engine_capacity' : self.get_engine_capacity(offer),
+            'horsepower' : self.get_horsepower(offer),
+            'body_type' : self.get_body_type(offer),
+            'drive_type' : self.get_drive_type(offer),
+            'engine_type' : self.get_engine_type(offer),
+            'offer_location' : self.get_offer_location(offer),
+            'is_offer_vip' : self.is_offer_vip(offer),
+            'offer_url' : self.get_offer_url(offer),
+            'marketplace' : self.marketplace_name,
+            'scraping_time' : date.today()
+        }
+        
+    def get_price(self, offer):
+        raise Exception('Not Implemented')
+            
+    def get_name(self, offer):
+        raise Exception('Not Implemented')
+            
+    def get_year(self, offer):
+        raise Exception('Not Implemented')
+            
+    def get_mileage(self, offer):
+        raise Exception('Not Implemented')
+            
+    def get_engine_capacity(self, offer):
+        raise Exception('Not Implemented')
+            
+    def get_horsepower(self, offer):
+        raise Exception('Not Implemented')
+            
+    def get_body_type(self, offer):
+        raise Exception('Not Implemented')
+            
+    def get_drive_type(self, offer):
+        raise Exception('Not Implemented')
+            
+    def get_engine_type(self, offer):
+        raise Exception('Not Implemented')
+            
+    def get_offer_location(self, offer):
+        raise Exception('Not Implemented')
+            
+    def is_offer_vip(self, offer):
+        raise Exception('Not Implemented')
+            
+    def get_offer_url(self, offer):
+        raise Exception('Not Implemented')    
+        
+    def save_cars_info(self, cars_info):
+        need_headers = not os.path.exists(self.data_file)
+        
+        with open(self.data_file, 'a') as f:
+            writer = csv.DictWriter(f, delimiter=',', lineterminator='\n', fieldnames = self.headers)
+            
+            if need_headers:
+                writer.writeheader()
+                
+            for car_info in cars_info:
+                writer.writerow(car_info)
